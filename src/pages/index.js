21814e74af57dc438334f54c69860f45f1dcace5
pandaLabOS <u6215112@au.edu>
2023-03-18 19:28:34 +0700

    Removed cat image because she's shy and won't show

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,12 +11,6 @@ export default function Home() {
             </Head>
             <Card style={{ width: "20rem", height : "fit-content"}}>
                 <Card.Body>
-                    <Image 
-                        src="/../public/images/cute-cat.jpeg" 
-                        width = "290"
-                        height = "180"
-                        alt = "cute cat image"
-                    />
                     <br/><br/>
                     <Card.Title>Go to Supplier Module</Card.Title>
                     <Card.Text>
